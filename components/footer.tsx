e7dc8ce0512a8fe77495550fc189bc82c1359f26
Martin Varmuza <mvarmuza@gmail.com>
2023-11-19 16:20:40 +0100

    wip

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,18 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            T0TALBRUTAL
-          </h3>
+          <div className="text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight ">
+              T0TALBRUTAL
+            </h3>
+            <div>
+              Veletrzni 826/61 <br />
+              170 00 Praha 7
+            </div>
+          </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            info@totalbrutal.cz
+            info@totalbrutal.cz <br/>
+            +420 721761947
           </div>
         </div>
       </Container>
